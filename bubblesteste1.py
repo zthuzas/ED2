2145590a9234830f0bd24d4f9d51983b72a00b3b
Arthur Xavier <131879335+zthuzas@users.noreply.github.com>
2025-03-22 15:52:55 -0300

    Add files via upload

---
+++ bubblesteste1.py
@@ -0,0 +1,9 @@
+#import random
+from bubblesort import bubblesort
+
+lista = [11, 4, 30, 22, 7, 26]
+print("Lista Desorndenada: ", lista)
+print("")
+
+bubblesort(lista)
+print("Lista ordenada: ", lista)
