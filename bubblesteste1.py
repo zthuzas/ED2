ffa1ab5e6442a6e3e57d8cf3b5b4d9747754a6bf
Arthur Xavier <131879335+zthuzas@users.noreply.github.com>
2025-03-22 16:12:41 -0300

    Delete bubblesteste1.py

--- bubblesteste1.py
+++
@@ -1,9 +0,0 @@
-#import random
-from bubblesort import bubblesort
-
-lista = [11, 4, 30, 22, 7, 26]
-print("Lista Desorndenada: ", lista)
-print("")
-
-bubblesort(lista)
-print("Lista ordenada: ", lista)
